044bc1205f5f38debc0cc2da0986529b69e3313a
paweldigital <173694350+paweldigital@users.noreply.github.com>
2025-09-26 15:51:55 +0100

    APB-9916: sync with frontend
    Signed-off-by: paweldigital <173694350+paweldigital@users.noreply.github.com>

---
+++ app/uk/gov/hmrc/agentregistration/shared/SafeId.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentregistration.shared
+
+import play.api.libs.functional.syntax.*
+import play.api.libs.json.Format
+
+/** SafeId (aka BusinessPartnerId in Etmp systems), is an unique identifier retrieved from the Grs that identifies the business entity being currently
+  * registered.
+  */
+final case class SafeId(value: String)
+
+object SafeId:
+  given format: Format[SafeId] = summon[Format[String]].inmap(SafeId(_), _.value)
