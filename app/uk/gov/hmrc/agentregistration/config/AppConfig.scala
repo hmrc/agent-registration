f11ea0a10ffeb6edc028371cee5c75254c9d0351
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-12-05 10:45:15 +0000

    APB-10402 - wip

--- app/uk/gov/hmrc/agentregistration/config/AppConfig.scala
+++ app/uk/gov/hmrc/agentregistration/config/AppConfig.scala
@@ -42,3 +42,6 @@ class AppConfig @Inject() (
 
   object ProvideDetailsRepo:
     val ttl: FiniteDuration = ConfigHelper.readFiniteDuration("mongodb.providedetails-repo-ttl", servicesConfig)
+
+  object UploadDetailsRepo:
+    val ttl: FiniteDuration = ConfigHelper.readFiniteDuration("mongodb.uploaddetails-repo-ttl", servicesConfig)
