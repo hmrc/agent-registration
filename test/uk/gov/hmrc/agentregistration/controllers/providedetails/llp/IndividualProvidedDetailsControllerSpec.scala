be14e8695a419a2b189db706bfe27915008d26a1
Mathew May <29102292+mghmay@users.noreply.github.com>
2026-02-26 12:19:20 +0000

    APB-10685 Add test for findByPersonReference endpoint

--- test/uk/gov/hmrc/agentregistration/controllers/providedetails/llp/IndividualProvidedDetailsControllerSpec.scala
+++ test/uk/gov/hmrc/agentregistration/controllers/providedetails/llp/IndividualProvidedDetailsControllerSpec.scala
@@ -18,22 +18,23 @@ package uk.gov.hmrc.agentregistration.controllers.providedetails.llp
 
 import play.api.http.Status
 import play.api.libs.json.Json
+import play.api.libs.ws.JsonBodyWritables.given
 import play.api.mvc.Request
+import uk.gov.hmrc.agentregistration.model.IndividualAddDetailsResponse
+import uk.gov.hmrc.agentregistration.repository.AgentApplicationRepo
 import uk.gov.hmrc.agentregistration.repository.providedetails.llp.IndividualProvidedDetailsRepo
+import uk.gov.hmrc.agentregistration.shared.ApplicationState.Submitted
+import uk.gov.hmrc.agentregistration.shared.individual.IndividualProvidedDetails
 import uk.gov.hmrc.agentregistration.testsupport.ControllerSpec
 import uk.gov.hmrc.agentregistration.testsupport.testdata.TdAll.tdAll.agentApplicationId
-import uk.gov.hmrc.agentregistration.testsupport.testdata.TdAll.tdAll.internalUserId
-import uk.gov.hmrc.agentregistration.testsupport.wiremock.stubs.providedetails.IndividualAuthStubs
+import uk.gov.hmrc.agentregistration.testsupport.testdata.TdAll.tdAll.individualProvidedDetailsId
 import uk.gov.hmrc.agentregistration.testsupport.wiremock.stubs.AuthStubs
+import uk.gov.hmrc.agentregistration.testsupport.wiremock.stubs.providedetails.IndividualAuthStubs
 import uk.gov.hmrc.agentregistration.util.RequestSupport.hc
 import uk.gov.hmrc.http.HttpReads.Implicits.given
 import uk.gov.hmrc.http.HttpReads
 import uk.gov.hmrc.http.HttpResponse
 import uk.gov.hmrc.http.StringContextOps
-import play.api.libs.ws.JsonBodyWritables.given
-import uk.gov.hmrc.agentregistration.shared.AgentApplicationId
-import uk.gov.hmrc.agentregistration.shared.individual.IndividualProvidedDetails
-import uk.gov.hmrc.agentregistration.shared.individual.IndividualProvidedDetailsId
 
 class IndividualProvidedDetailsControllerSpec
 extends ControllerSpec:
@@ -73,6 +74,35 @@ extends ControllerSpec:
     val individualProvidedDetails = response.json.as[List[IndividualProvidedDetails]]
     individualProvidedDetails shouldBe List(individualProvidedDetailsStarted)
     IndividualAuthStubs.verifyAuthorise()
+
+  "find by person reference returns Ok and IndividualAddDetailsResponse as Json body" in:
+
+    given Request[?] = tdAll.backendRequest
+    AuthStubs.stubAuthorise()
+    val individualProvidedDetailsRepo: IndividualProvidedDetailsRepo = app.injector.instanceOf[IndividualProvidedDetailsRepo]
+    val individualProvidedDetails = tdAll.providedDetailsLlp.afterStarted
+    individualProvidedDetailsRepo.upsert(individualProvidedDetails).futureValue
+    val applicationRepo = app.injector.instanceOf[AgentApplicationRepo]
+    val agentApplication = tdAll.llpApplicationAfterCreated.copy(applicationState = Submitted)
+    applicationRepo.upsert(agentApplication).futureValue
+
+    individualProvidedDetailsRepo.findById(
+      individualProvidedDetails.individualProvidedDetailsId
+    ).futureValue.value shouldBe individualProvidedDetails withClue "sanity check"
+
+    applicationRepo.findById(
+      agentApplication.agentApplicationId
+    ).futureValue.value shouldBe agentApplication withClue "sanity check"
+
+    val response =
+      httpClient
+        .get(url"$baseUrl/agent-registration/individual-provided-details/by-person-reference/${individualProvidedDetailsId.value}")
+        .execute[HttpResponse]
+        .futureValue
+    response.status shouldBe Status.OK
+    val individualAddDetailsResponse = response.json.as[IndividualAddDetailsResponse]
+    individualAddDetailsResponse.individualProvidedDetailsId shouldBe individualProvidedDetailsId
+    individualAddDetailsResponse.linkId shouldBe agentApplication.linkId
 
   "upsert with PUT individual provided details to mongo and returns OK" in:
 
