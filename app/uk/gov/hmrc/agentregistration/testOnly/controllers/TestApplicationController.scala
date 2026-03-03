2ae8cff6fb9c82ca1c9a3dea59034b2275fa016c
Mathew May <29102292+mghmay@users.noreply.github.com>
2026-03-03 14:15:24 +0000

    APB-10685-2 Add POST and DELETE test only endpoints for smu individual (#100)
    * APB-10685-2 Add POST and DELETE test only endpoints for smu individual
    
    * APB-10685-2 Change Ok status to Created on POST
    
    * APB-10685-2 Add TODO to handle stubbed data better in the future

--- app/uk/gov/hmrc/agentregistration/testOnly/controllers/TestApplicationController.scala
+++ app/uk/gov/hmrc/agentregistration/testOnly/controllers/TestApplicationController.scala
@@ -22,32 +22,22 @@ import play.api.mvc.AnyContent
 import play.api.mvc.ControllerComponents
 import uk.gov.hmrc.agentregistration.action.Actions
 import uk.gov.hmrc.agentregistration.repository.AgentApplicationRepo
-import uk.gov.hmrc.agentregistration.shared.AgentApplication
-import uk.gov.hmrc.agentregistration.shared.AgentApplicationIdGenerator
-import uk.gov.hmrc.agentregistration.shared.AgentApplicationLlp
-import uk.gov.hmrc.agentregistration.shared.ApplicationState
-import uk.gov.hmrc.agentregistration.shared.CheckResult
-import uk.gov.hmrc.agentregistration.shared.Crn
-import uk.gov.hmrc.agentregistration.shared.EmailAddress
-import uk.gov.hmrc.agentregistration.shared.GroupId
-import uk.gov.hmrc.agentregistration.shared.InternalUserId
-import uk.gov.hmrc.agentregistration.shared.LinkId
-import uk.gov.hmrc.agentregistration.shared.PayeRef
-import uk.gov.hmrc.agentregistration.shared.SaUtr
-import uk.gov.hmrc.agentregistration.shared.SafeId
-import uk.gov.hmrc.agentregistration.shared.StateOfAgreement
-import uk.gov.hmrc.agentregistration.shared.TelephoneNumber
-import uk.gov.hmrc.agentregistration.shared.UserRole
-import uk.gov.hmrc.agentregistration.shared.Vrn
+import uk.gov.hmrc.agentregistration.repository.providedetails.llp.IndividualProvidedDetailsRepo
+import uk.gov.hmrc.agentregistration.shared.*
+import uk.gov.hmrc.agentregistration.shared.ApplicationState.Started
 import uk.gov.hmrc.agentregistration.shared.CheckResult.Pass
 import uk.gov.hmrc.agentregistration.shared.businessdetails.BusinessDetailsLlp
 import uk.gov.hmrc.agentregistration.shared.businessdetails.CompanyProfile
 import uk.gov.hmrc.agentregistration.shared.contactdetails.ApplicantContactDetails
 import uk.gov.hmrc.agentregistration.shared.contactdetails.ApplicantEmailAddress
 import uk.gov.hmrc.agentregistration.shared.contactdetails.ApplicantName
+import uk.gov.hmrc.agentregistration.shared.individual.*
+import uk.gov.hmrc.agentregistration.shared.lists.IndividualName
+import uk.gov.hmrc.agentregistration.shared.util.Errors.getOrThrowExpectedDataMissing
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
 import java.time.Instant
+import java.time.LocalDate
 import java.util.UUID
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -58,11 +48,37 @@ class TestApplicationController @Inject() (
   cc: ControllerComponents,
   actions: Actions,
   agentApplicationRepo: AgentApplicationRepo,
-  agentApplicationIdGenerator: AgentApplicationIdGenerator
+  agentApplicationIdGenerator: AgentApplicationIdGenerator,
+  individualProvidedDetailsRepo: IndividualProvidedDetailsRepo,
+  individualProvidedDetailsIdGenerator: IndividualProvidedDetailsIdGenerator
 )
 extends BackendController(cc):
 
   given ExecutionContext = controllerComponents.executionContext
+
+  def createTestSmuIndividual: Action[AnyContent] = Action
+    .async:
+      implicit request =>
+        val agentApplication: AgentApplication = makeApplicationToProvideDetailsFor(applicationState = ApplicationState.Submitted)
+        val individual: IndividualProvidedDetails = makeIndividualProvidedDetailsFor(agentApplication.agentApplicationId)
+        for
+          _ <- agentApplicationRepo.upsert(agentApplication)
+          _ <- individualProvidedDetailsRepo.upsert(individual)
+        yield Created(Json.obj(
+          "individualProvidedDetailsId" -> individual.individualProvidedDetailsId.value
+        ))
+
+  def deleteTestSmuIndividual(individualId: IndividualProvidedDetailsId): Action[AnyContent] = Action
+    .async:
+      implicit request =>
+        for
+          maybeIndividual <- individualProvidedDetailsRepo.findById(individualId)
+          applicationId = maybeIndividual.map(_.agentApplicationId).getOrThrowExpectedDataMissing("agentApplicationId")
+          maybeAaRes <- agentApplicationRepo.removeById(applicationId)
+          maybeIpdRes <- individualProvidedDetailsRepo.removeById(individualId)
+        yield (maybeAaRes, maybeIpdRes) match
+          case (Some(aaRes), Some(ipdRes)) if aaRes.wasAcknowledged() && ipdRes.wasAcknowledged() => NoContent
+          case _ => BadRequest("Something went wrong")
 
   def createTestApplication: Action[AnyContent] = Action
     .async:
@@ -72,13 +88,14 @@ extends BackendController(cc):
           .upsert(agentApplication)
           .map(_ => Ok(Json.obj("linkId" -> agentApplication.linkId.value)))
 
-  private def makeApplicationToProvideDetailsFor(): AgentApplication = AgentApplicationLlp(
+  // TODO: We should revisit the way that we handle the stubbing here after we have brought test data into the shared space
+  private def makeApplicationToProvideDetailsFor(applicationState: ApplicationState = Started): AgentApplication = AgentApplicationLlp(
     _id = agentApplicationIdGenerator.nextApplicationId(),
     linkId = LinkId(value = UUID.randomUUID().toString),
     internalUserId = InternalUserId(value = s"test-${UUID.randomUUID().toString}"),
     groupId = GroupId(value = UUID.randomUUID().toString),
     createdAt = Instant.now(),
-    applicationState = ApplicationState.Started, // Provide details journeys now happen before an application is finished
+    applicationState = applicationState, // Provide details journeys now happen before an application is finished
     userRole = Some(UserRole.Authorised),
     businessDetails = Some(BusinessDetailsLlp(
       safeId = SafeId("safe-id-12345"),
@@ -105,6 +122,25 @@ extends BackendController(cc):
     hasOtherRelevantIndividuals = None,
     refusalToDealWithCheckResult = Some(Pass),
     companyStatusCheckResult = Some(Pass),
+    vrns = Some(List(Vrn("12341234"), Vrn("43214321"))),
+    payeRefs = Some(List(PayeRef("56785678"), PayeRef("87658765")))
+  )
+  // TODO: Same as makeApplicationToProvideDetailsFor, we should use test data here or create FF links to populate this data
+  private def makeIndividualProvidedDetailsFor(agentApplicationId: AgentApplicationId): IndividualProvidedDetails = IndividualProvidedDetails(
+    _id = individualProvidedDetailsIdGenerator.nextIndividualProvidedDetailsId(),
+    individualName = IndividualName("George Smiley"),
+    isPersonOfControl = true,
+    internalUserId = Some(InternalUserId(value = s"test-${UUID.randomUUID().toString}")),
+    createdAt = Instant.now(),
+    providedDetailsState = ProvidedDetailsState.Finished,
+    agentApplicationId = agentApplicationId,
+    individualDateOfBirth = Some(IndividualDateOfBirth.Provided(LocalDate.of(1980, 1, 1))),
+    telephoneNumber = Some(TelephoneNumber("1234658979")),
+    emailAddress = Some(IndividualVerifiedEmailAddress(EmailAddress("g.smiley@test.com"), isVerified = true)),
+    individualNino = Some(IndividualNino.Provided(Nino("AA123456A"))),
+    individualSaUtr = Some(IndividualSaUtr.Provided(SaUtr("1234567890"))),
+    hmrcStandardForAgentsAgreed = StateOfAgreement.Agreed,
+    hasApprovedApplication = Some(true),
     vrns = Some(List(Vrn("12341234"), Vrn("43214321"))),
     payeRefs = Some(List(PayeRef("56785678"), PayeRef("87658765")))
   )
