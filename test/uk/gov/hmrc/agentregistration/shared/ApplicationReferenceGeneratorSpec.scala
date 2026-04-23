44d4c7a810c2d6de0662bde38616cdd8cae0f05a
Martin Rolfe <52818407+mrolfe88@users.noreply.github.com>
2026-04-23 15:44:06 +0100

    [APB-10394] Make the application reference more user-friendly (#136)
    * [APB-10394] Make the application reference more user-friendly
    
    * [APB-10394] Make the application reference more user-friendly
    
    * [APB-10394] Make the application reference more user-friendly
    
    * [APB-10394] Make the application reference more user-friendly
    
    * [APB-10394] Make the application reference more user-friendly
    
    * [APB-10394] Make the application reference more user-friendly
    
    * [APB-10394] Make the application reference more user-friendly
    
    * [APB-10394] Add suppress warning

---
+++ test/uk/gov/hmrc/agentregistration/shared/ApplicationReferenceGeneratorSpec.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentregistration.shared
+
+import uk.gov.hmrc.agentregistration.testsupport.UnitSpec
+
+class ApplicationReferenceGeneratorSpec
+extends UnitSpec:
+
+  "generateApplicationReference should create unique references in the agreed format" in:
+
+    val generator: ApplicationReferenceGenerator = new ApplicationReferenceGenerator
+    val formatRegex: String = "^[A-HJ-NP-RTXYZ2346789]{9}$"
+
+    val ref1: ApplicationReference = generator.generateApplicationReference()
+    val ref2: ApplicationReference = generator.generateApplicationReference()
+    ref1 should not be ref2
+    ref1.value.length shouldBe 9
+    ref2.value.length shouldBe 9
+    ref1.value.matches(formatRegex) shouldBe true
+    ref2.value.matches(formatRegex) shouldBe true
